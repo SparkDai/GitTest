c43e7fcc984e302a6aba24064591b6f470cdb8d4
daishenao <daishenao@flowcapai.com>
2022-06-05 22:50:46 +0800

    Hello Today 的颜色改为绿色

--- GitTest/GitTest/ViewController.swift
+++ GitTest/GitTest/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         
         let helloWorldLabel = UILabel()
         helloWorldLabel.text = "Hello Today!"
-        helloWorldLabel.textColor = .red
+        helloWorldLabel.textColor = .green
         helloWorldLabel.font = UIFont.systemFont(ofSize: 32)
         
         view.addSubview(helloWorldLabel)
